9cde6feed42ae35d73d2b5fda09f62c9d3e2f446
Cyril Galibern <cyril.galibern@opensvc.com>
2024-02-06 14:38:24 +0100

    [auth] Implement public strategy

---
+++ auth/public.go
@@ -0,0 +1,31 @@
+package auth
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"strings"
+
+	"github.com/shaj13/go-guardian/v2/auth"
+)
+
+type (
+	public struct {
+		prefix string
+	}
+)
+
+var (
+	ErrPrivatePath = errors.New("not public url")
+)
+
+func NewPublicStrategy(s string) auth.Strategy {
+	return &public{prefix: s}
+}
+
+func (p *public) Authenticate(_ context.Context, r *http.Request) (auth.Info, error) {
+	if strings.HasPrefix(r.RequestURI, "/public/") {
+		return auth.NewUserInfo("public", "", nil, nil), nil
+	}
+	return nil, ErrPrivatePath
+}
