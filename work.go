3d435bb04f2cd5ca430bb2cdd780cabea189412c
Cyril Galibern <cyril.galibern@opensvc.com>
2024-04-17 12:05:52 +0200

    [worker] listen pprof on /var/run/oc3-worker.soc

--- work.go
+++ work.go
@@ -1,6 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"log/slog"
+	"net"
+	"net/http"
+	_ "net/http/pprof"
+	"os"
+
 	"github.com/spf13/viper"
 
 	"github.com/opensvc/oc3/oc2websocket"
@@ -25,5 +32,28 @@ func work(queues []string) error {
 	if err != nil {
 		return err
 	}
+	if viper.GetBool("worker.pprof.enable") {
+		if err := workerPprof(viper.GetString("worker.pprof.uxsocket")); err != nil {
+			return err
+		}
+	}
 	return w.Run()
+}
+
+func workerPprof(p string) error {
+	slog.Info(fmt.Sprintf("pprof listener on %s", p))
+	if err := os.RemoveAll(p); err != nil {
+		return err
+	}
+	listener, err := net.Listen("unix", p)
+	if err != nil {
+		return err
+	}
+	go func() {
+		server := http.Server{}
+		if err := server.Serve(listener); err != nil {
+			slog.Error(fmt.Sprintf("worker ux listener: %s", err))
+		}
+	}()
+	return nil
 }
