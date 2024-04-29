f84eb593ce6836061c96c0f08608b57221462698
Cyril Galibern <cyril.galibern@opensvc.com>
2024-04-29 16:17:32 +0200

    [api] Don't serve api schema when listener.ui.enable is false

--- handlers/main.go
+++ handlers/main.go
@@ -3,12 +3,26 @@ package handlers
 import (
 	"database/sql"
 
+	"github.com/getkin/kin-openapi/openapi3"
 	"github.com/go-redis/redis/v8"
+
+	"github.com/opensvc/oc3/api"
 )
 
 type (
 	Api struct {
 		DB    *sql.DB
 		Redis *redis.Client
+		UI    bool
 	}
 )
+
+var (
+	SCHEMA openapi3.T
+)
+
+func init() {
+	if schema, err := api.GetSwagger(); err == nil {
+		SCHEMA = *schema
+	}
+}
