ced27e7ba42424c046f1c63b465b773604378d76
Cyril Galibern <cyril.galibern@opensvc.com>
2024-06-12 18:04:56 +0200

    [worker] Implements object config job

---
+++ worker/job_feed_object_config.go
@@ -0,0 +1,113 @@
+package worker
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/go-redis/redis/v8"
+
+	"github.com/opensvc/oc3/cachekeys"
+	"github.com/opensvc/oc3/mariadb"
+)
+
+type (
+	jobFeedObjectConfig struct {
+		*BaseJob
+
+		// idX is the id of the posted object config with expected pattern: <objectName>@<nodeID>@<clusterID>
+		idX string
+
+		objectName string
+
+		// nodeID is db ID of the node that have posted object config
+		nodeID string
+
+		// clusterID is the db cluster ID of the node that have posted object config
+		clusterID string
+
+		// objectID is db ID of the service with objectName and clusterID
+		objectID string
+
+		// data is the posted object config
+		data map[string]any
+	}
+)
+
+func newFeedObjectConfig(objectName, nodeID, clusterID string) *jobFeedObjectConfig {
+	idX := fmt.Sprintf("%s@%s@%s", objectName, nodeID, clusterID)
+	return &jobFeedObjectConfig{
+		BaseJob: &BaseJob{
+			name:   "objectConfig",
+			detail: "ID: " + idX,
+
+			cachePendingH:   cachekeys.FeedObjectConfigPendingH,
+			cachePendingIDX: idX,
+		},
+		idX:        idX,
+		nodeID:     nodeID,
+		clusterID:  clusterID,
+		objectName: objectName,
+	}
+}
+
+func (d *jobFeedObjectConfig) Operations() []operation {
+	return []operation{
+		{desc: "objectConfig/dropPending", do: d.dropPending},
+		{desc: "objectConfig/getData", do: d.getData},
+		{desc: "objectConfig/dbNow", do: d.dbNow},
+		{desc: "objectConfig/updateDB", do: d.updateDB},
+	}
+}
+
+func (d *jobFeedObjectConfig) getData() error {
+	cmd := d.redis.HGet(d.ctx, cachekeys.FeedObjectConfigH, d.idX)
+	result, err := cmd.Result()
+	switch err {
+	case nil:
+	case redis.Nil:
+		return fmt.Errorf("HGET: no results")
+	default:
+		return fmt.Errorf("HGET: %w", err)
+	}
+	if err := json.Unmarshal([]byte(result), &d.data); err != nil {
+		return fmt.Errorf("unmarshal: %w", err)
+	}
+	return nil
+}
+
+func (d *jobFeedObjectConfig) updateDB() (err error) {
+	_, d.objectID, err = d.oDb.objectIDFindOrCreate(d.ctx, d.objectName, d.clusterID)
+	if err != nil {
+		return err
+	}
+	d.data["svc_id"] = d.objectID
+	d.data["updated"] = d.now
+
+	if v, ok := d.data["topology"]; ok {
+		if topology, ok := v.(string); ok && topology == "ha" {
+			d.data["svc_ha"] = 1
+		} else {
+			d.data["svc_ha"] = 0
+		}
+	}
+	request := mariadb.InsertOrUpdate{
+		Table: "services",
+		Mappings: mariadb.Mappings{
+			mariadb.Mapping{From: "path", To: "svcname"},
+			mariadb.Mapping{To: "svc_id"},
+			mariadb.Mapping{From: "scope", To: "svc_nodes", Optional: true, Modify: mariadb.ModifierToString(" ")},
+			mariadb.Mapping{From: "app", To: "svc_app", Optional: true},
+			mariadb.Mapping{From: "env", To: "svc_env", Optional: true},
+			mariadb.Mapping{From: "comment", To: "svc_comment", Optional: true},
+			mariadb.Mapping{From: "FlexMin", To: "svc_flex_min_nodes", Optional: true},
+			mariadb.Mapping{From: "FlexMax", To: "svc_flex_max_nodes", Optional: true},
+			mariadb.Mapping{To: "svc_ha", Optional: true},
+			mariadb.Mapping{From: "raw_config", To: "svc_config", Optional: true, Modify: mariadb.ModifyFromBase64ToString},
+			mariadb.Mapping{To: "updated"},
+		},
+		Keys: []string{"svc_id"},
+		Data: d.data,
+	}
+	_, err = request.QueryContext(d.ctx, d.db)
+	return err
+}
