fab36db64c71f475525cdc2282ca9c8203b5bd6f
Cyril Galibern <cyril.galibern@opensvc.com>
2024-08-26 20:28:50 +0200

    [api,worker] feed daemon ping should not update timestamp of non cluster nodes, objects

--- apihandlers/post_feed_daemon_ping.go
+++ apihandlers/post_feed_daemon_ping.go
@@ -2,6 +2,7 @@ package apihandlers
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -27,6 +28,21 @@ func (a *Api) PostFeedDaemonPing(c echo.Context) error {
 		return c.NoContent(http.StatusNoContent)
 	}
 
+	body := c.Request().Body
+	b, err := io.ReadAll(body)
+	defer func() {
+		if err := body.Close(); err != nil {
+			log.Error("close body failed: " + err.Error())
+		}
+	}()
+	if err := a.Redis.HSet(ctx, cachekeys.FeedDaemonPingH, nodeID, string(b)).Err(); err != nil {
+		log.Error(fmt.Sprintf("HSET %s %s", cachekeys.FeedDaemonPingH, nodeID))
+		return JSONProblemf(c, http.StatusInternalServerError, "redis operation", "can't HSET %s: %s", cachekeys.FeedDaemonPingH, err)
+	}
+
+	if err != nil {
+		return JSONProblemf(c, http.StatusInternalServerError, "", "read request body: %s", err)
+	}
 	if err := a.pushNotPending(ctx, cachekeys.FeedDaemonPingPendingH, cachekeys.FeedDaemonPingQ, nodeID); err != nil {
 		log.Error(fmt.Sprintf("can't push %s %s: %s", cachekeys.FeedDaemonPingQ, nodeID, err))
 		return JSONProblemf(c, http.StatusInternalServerError, "redis operation", "can't push %s %s: %s", cachekeys.FeedDaemonPingQ, nodeID, err)
--- worker/job_feed_daemon_ping.go
+++ worker/job_feed_daemon_ping.go
@@ -1,9 +1,11 @@
 package worker
 
 import (
+	"encoding/json"
 	"fmt"
 	"log/slog"
 
+	"github.com/opensvc/oc3/api"
 	"github.com/opensvc/oc3/cachekeys"
 )
 
@@ -17,6 +19,12 @@ type (
 
 		byObjectID map[string]*DBObject
 		byNodeID   map[string]*DBNode
+
+		// clusterNode is a map of cluster nodes (from POST feed daemon ping)
+		clusterNode map[string]struct{}
+
+		// clusterObject is a map of object names (from POST feed daemon ping)
+		clusterObject map[string]struct{}
 	}
 )
 
@@ -33,12 +41,16 @@ func newDaemonPing(nodeID string) *jobFeedDaemonPing {
 
 		byNodeID:   make(map[string]*DBNode),
 		byObjectID: make(map[string]*DBObject),
+
+		clusterNode:   make(map[string]struct{}),
+		clusterObject: make(map[string]struct{}),
 	}
 }
 
 func (d *jobFeedDaemonPing) Operations() []operation {
 	return []operation{
 		{desc: "daemonPing/dropPending", do: d.dropPending},
+		{desc: "daemonPing/getData", do: d.getData},
 		{desc: "daemonPing/dbFetchNodes", do: d.dbFetchNodes},
 		{desc: "daemonPing/dbFetchObjects", do: d.dbFetchObjects},
 		{desc: "daemonPing/dbPingInstances", do: d.dbPingInstances},
@@ -46,6 +58,24 @@ func (d *jobFeedDaemonPing) Operations() []operation {
 		{desc: "daemonPing/cacheObjectsWithoutConfig", do: d.cacheObjectsWithoutConfig},
 		{desc: "daemonPing/pushFromTableChanges", do: d.pushFromTableChanges},
 	}
+}
+
+func (d *jobFeedDaemonPing) getData() error {
+	var data api.PostFeedDaemonPing
+	if b, err := d.redis.HGet(d.ctx, cachekeys.FeedDaemonPingH, d.nodeID).Bytes(); err != nil {
+		return fmt.Errorf("getData: HGET %s %s: %w", cachekeys.FeedDaemonPingH, d.nodeID, err)
+	} else if err = json.Unmarshal(b, &data); err != nil {
+		return fmt.Errorf("getData: unexpected data from %s %s: %w", cachekeys.FeedDaemonPingH, d.nodeID, err)
+	} else {
+		for _, nodename := range data.Nodes {
+			d.clusterNode[nodename] = struct{}{}
+		}
+
+		for _, objectName := range data.Objects {
+			d.clusterObject[objectName] = struct{}{}
+		}
+	}
+	return nil
 }
 
 // dbFetchNodes fetch nodes (that are associated with caller node ID) from database
@@ -58,6 +88,10 @@ func (d *jobFeedDaemonPing) dbFetchNodes() (err error) {
 		return fmt.Errorf("dbFetchNodes %s: %w", d.nodeID, err)
 	}
 	for _, n := range dbNodes {
+		if _, ok := d.clusterNode[n.nodename]; !ok {
+			// skipped: not member of posted cluster nodenames
+			continue
+		}
 		d.byNodeID[n.nodeID] = n
 	}
 	callerNode, ok := d.byNodeID[d.nodeID]
@@ -78,6 +112,10 @@ func (d *jobFeedDaemonPing) dbFetchObjects() (err error) {
 			d.callerNode.nodename, d.nodeID, d.clusterID, err)
 	}
 	for _, o := range objects {
+		if _, ok := d.clusterObject[o.svcname]; !ok {
+			// skipped: not member of posted object names
+			continue
+		}
 		d.byObjectID[o.svcID] = o
 		slog.Debug(fmt.Sprintf("dbFetchObjects  %s (%s)", o.svcname, o.svcID))
 	}
@@ -112,5 +150,9 @@ func (d *jobFeedDaemonPing) dbPingObjects() (err error) {
 
 // cacheObjectsWithoutConfig populate FeedObjectConfigForClusterIDH with names of objects without config
 func (d *jobFeedDaemonPing) cacheObjectsWithoutConfig() error {
-	return d.populateFeedObjectConfigForClusterIDH(d.clusterID, d.byObjectID)
+	objects, err := d.populateFeedObjectConfigForClusterIDH(d.clusterID, d.byObjectID)
+	if len(objects) > 0 {
+		slog.Info(fmt.Sprintf("daemonPing nodeID: %s need object config: %s", d.nodeID, objects))
+	}
+	return err
 }
