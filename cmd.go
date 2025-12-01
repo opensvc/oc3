52490d5abc167f191347987d26895720d42809aa
Christophe Varoqui <christophe.varoqui@opensvc.com>
2025-12-01 10:34:06 +0100

    Add the "oc3 version" command

--- cmd.go
+++ cmd.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/spf13/cobra"
 
+	"github.com/opensvc/oc3/scheduler"
 	"github.com/opensvc/oc3/util/version"
 )
 
@@ -20,6 +21,9 @@ func cmdWorker() *cobra.Command {
 		Use:   "worker",
 		Short: "run jobs from a list of queues",
 		RunE: func(cmd *cobra.Command, args []string) error {
+			if err := setup(); err != nil {
+				return err
+			}
 			return work(maxRunners, args)
 		},
 	}
@@ -32,32 +36,61 @@ func cmdAPI() *cobra.Command {
 		Use:   "api",
 		Short: "serve the collector api",
 		RunE: func(cmd *cobra.Command, args []string) error {
+			if err := setup(); err != nil {
+				return err
+			}
 			return listen()
 		},
 	}
+}
+
+func cmdScheduler() *cobra.Command {
+	return &cobra.Command{
+		Use:   "scheduler",
+		Short: "start running db maintenance tasks",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if err := setup(); err != nil {
+				return err
+			}
+			return scheduler.Run()
+		},
+	}
+}
+
+func cmdVersion() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "version",
+		Short: "display the oc3 version",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println(version.Version())
+		},
+	}
+	return cmd
+}
+
+func setup() error {
+	if debug {
+		slog.SetLogLoggerLevel(slog.LevelDebug)
+	}
+	logConfigDir()
+	if err := initConfig(); err != nil {
+		return err
+	}
+	logConfigFileUsed()
+	slog.Info(fmt.Sprintf("oc3 version: %s", version.Version()))
+	return nil
 }
 
 func cmdRoot(args []string) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   filepath.Base(args[0]),
 		Short: "Manage the opensvc collector infrastructure components.",
-		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-			if debug {
-				slog.SetLogLoggerLevel(slog.LevelDebug)
-			}
-
-			logConfigDir()
-			if err := initConfig(); err != nil {
-				return err
-			}
-			logConfigFileUsed()
-			slog.Info(fmt.Sprintf("oc3 vesion: %s", version.Version()))
-			return nil
-		},
 	}
 	cmd.PersistentFlags().BoolVar(&debug, "debug", false, "set log level to debug")
 	cmd.AddCommand(
 		cmdAPI(),
+		cmdScheduler(),
+		cmdVersion(),
 		cmdWorker(),
 	)
 	return cmd
