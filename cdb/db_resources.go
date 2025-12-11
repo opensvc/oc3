a9afb0c2d299204a175cfb7f02c53cca33686209
Christophe Varoqui <christophe.varoqui@opensvc.com>
2025-12-11 10:23:27 +0100

    Fix and simplify the resmon scrub task

--- cdb/db_resources.go
+++ cdb/db_resources.go
@@ -10,7 +10,20 @@ import (
 	"github.com/google/uuid"
 )
 
-func (oDb *DB) ResourceOutdatedLists(ctx context.Context) (ids []int64, rids []string, svcIDs []uuid.UUID, nodeIDs []uuid.UUID, err error) {
+type (
+	ResourceMeta struct {
+		ID  int64
+		OID uuid.UUID
+		NID uuid.UUID
+		RID string
+	}
+)
+
+func (t ResourceMeta) String() string {
+	return fmt.Sprintf("%s@%s:%s", t.OID, t.NID, t.RID)
+}
+
+func (oDb *DB) ResourceOutdatedLists(ctx context.Context) (resources []ResourceMeta, err error) {
 	sql := `SELECT id, rid, svc_id, node_id FROM resmon
                 WHERE updated < DATE_SUB(NOW(), INTERVAL 15 MINUTE)
                 AND res_status != "undef"`
@@ -25,14 +38,9 @@ func (oDb *DB) ResourceOutdatedLists(ctx context.Context) (ids []int64, rids []s
 		if !next {
 			break
 		}
-		var svcID uuid.UUID
-		var nodeID uuid.UUID
-		var rid string
-		var id int64
-		rows.Scan(&id, &rid, &svcID, &nodeID)
-		svcIDs = append(svcIDs, svcID)
-		nodeIDs = append(nodeIDs, nodeID)
-		rids = append(rids, rid)
+		var resource ResourceMeta
+		rows.Scan(&resource.ID, &resource.RID, &resource.OID, &resource.NID)
+		resources = append(resources, resource)
 	}
 	return
 }
@@ -41,19 +49,18 @@ func (oDb *DB) ResourceOutdatedLists(ctx context.Context) (ids []int64, rids []s
 //
 // resmon_log_last tracks the current avail value from begin to now.
 // resmon_log tracks avail values changes with begin and end: [(avail, begin, end), ...]
-func (oDb *DB) ResourceUpdateLog(ctx context.Context, rid string, svcID, nodeID uuid.UUID, avail string) error {
-	name := fmt.Sprintf("%s@%s:%s", svcID, nodeID, rid)
-	defer logDuration("ResourceUpdateLog "+name, time.Now())
+func (oDb *DB) ResourceUpdateLog(ctx context.Context, resource ResourceMeta, avail string) error {
+	defer logDuration("ResourceUpdateLog "+resource.String(), time.Now())
 	const (
-		qGetLogLast = "SELECT `res_status`, `res_begin` FROM `resmon_log_last` WHERE `res_id` = ?"
+		qGetLogLast = "SELECT `res_status`, `res_begin` FROM `resmon_log_last` WHERE `svc_id` = ? AND `node_id` = ? AND `rid` = ?"
 		qSetLogLast = "" +
-			"INSERT INTO `resmon_log_last` (`svc_id`, `res_begin`, `res_end`, `res_status`)" +
-			" VALUES (?, NOW(), NOW(), ?)" +
+			"INSERT INTO `resmon_log_last` (`svc_id`, `node_id`, `rid`, `res_begin`, `res_end`, `res_status`)" +
+			" VALUES (?, ?, ?, NOW(), NOW(), ?)" +
 			" ON DUPLICATE KEY UPDATE `res_begin` = NOW(), `res_end` = NOW(), `res_status` = ?"
-		qExtendIntervalOfCurrentAvail                = "UPDATE `resmon_log_last` SET `res_end` = NOW() WHERE `svc_id` = ?"
+		qExtendIntervalOfCurrentAvail                = "UPDATE `resmon_log_last` SET `res_end` = NOW() WHERE `svc_id` = ? AND `node_id` = ? AND `rid` = ?"
 		qSaveIntervalOfPreviousAvailBeforeTransition = "" +
-			"INSERT INTO `resmon_log` (`svc_id`, `res_begin`, `res_end`, `res_status`)" +
-			" VALUES (?, ?, NOW(), ?)"
+			"INSERT INTO `resmon_log` (`svc_id`, `node_id`, `rid`, `res_begin`, `res_end`, `res_status`)" +
+			" VALUES (?, ?, ?, ?, NOW(), ?)"
 	)
 	var (
 		previousAvail string
@@ -61,32 +68,32 @@ func (oDb *DB) ResourceUpdateLog(ctx context.Context, rid string, svcID, nodeID 
 		previousBegin time.Time
 	)
 	setLogLast := func() error {
-		_, err := oDb.DB.ExecContext(ctx, qSetLogLast, svcID, avail, avail)
+		_, err := oDb.DB.ExecContext(ctx, qSetLogLast, resource.OID, resource.NID, resource.RID, avail, avail)
 		if err != nil {
-			return fmt.Errorf("objectUpdateLog can't update resmon_log_last %s: %w", svcID, err)
+			return fmt.Errorf("objectUpdateLog can't update resmon_log_last %s: %w", resource, err)
 		}
 		return nil
 	}
-	err := oDb.DB.QueryRowContext(ctx, qGetLogLast, svcID).Scan(&previousAvail, &previousBegin)
+	err := oDb.DB.QueryRowContext(ctx, qGetLogLast, resource.OID, resource.NID, resource.RID).Scan(&previousAvail, &previousBegin)
 	switch {
 	case errors.Is(err, sql.ErrNoRows):
 		// set initial avail value
 		defer oDb.SetChange("resmon_log")
 		return setLogLast()
 	case err != nil:
-		return fmt.Errorf("objectUpdateLog can't get resmon_log_last %s: %w", svcID, err)
+		return fmt.Errorf("objectUpdateLog can't get resmon_log_last %s: %w", resource, err)
 	default:
 		defer oDb.SetChange("resmon_log")
 		if previousAvail == avail {
 			// no change, extend last interval
-			if _, err := oDb.DB.ExecContext(ctx, qExtendIntervalOfCurrentAvail, svcID); err != nil {
-				return fmt.Errorf("objectUpdateLog can't set resmon_log_last.res_end %s: %w", svcID, err)
+			if _, err := oDb.DB.ExecContext(ctx, qExtendIntervalOfCurrentAvail, resource.OID, resource.NID, resource.RID); err != nil {
+				return fmt.Errorf("objectUpdateLog can't set resmon_log_last.res_end %s: %w", resource, err)
 			}
 			return nil
 		} else {
 			// the avail value will change, save interval of previous avail value before change
-			if _, err := oDb.DB.ExecContext(ctx, qSaveIntervalOfPreviousAvailBeforeTransition, svcID, previousBegin, previousAvail); err != nil {
-				return fmt.Errorf("objectUpdateLog can't save resmon_log change %s: %w", svcID, err)
+			if _, err := oDb.DB.ExecContext(ctx, qSaveIntervalOfPreviousAvailBeforeTransition, resource.OID, resource.NID, resource.RID, previousBegin, previousAvail); err != nil {
+				return fmt.Errorf("objectUpdateLog can't save resmon_log change %s: %w", resource, err)
 			}
 			// reset begin and end interval for new avail
 			return setLogLast()
@@ -94,8 +101,8 @@ func (oDb *DB) ResourceUpdateLog(ctx context.Context, rid string, svcID, nodeID 
 	}
 }
 
-func (oDb *DB) ResourceUpdateStatus(ctx context.Context, ids []int64, status string) (n int64, err error) {
-	idsLen := len(ids)
+func (oDb *DB) ResourceUpdateStatus(ctx context.Context, resources []ResourceMeta, status string) (n int64, err error) {
+	idsLen := len(resources)
 	sql := `UPDATE resmon
                 SET res_status=?, updated=NOW()
                 WHERE id IN (%s)`
@@ -103,8 +110,8 @@ func (oDb *DB) ResourceUpdateStatus(ctx context.Context, ids []int64, status str
 
 	args := make([]any, idsLen+1)
 	args[0] = status
-	for i, id := range ids {
-		args[i+1] = id
+	for i, resource := range resources {
+		args[i+1] = resource.ID
 	}
 
 	result, err := oDb.DB.ExecContext(ctx, sql, args...)
--- scheduler/task_scrub.go
+++ scheduler/task_scrub.go
@@ -3,7 +3,6 @@ package scheduler
 import (
 	"context"
 	"errors"
-	"fmt"
 	"time"
 
 	"github.com/opensvc/oc3/cdb"
@@ -23,17 +22,17 @@ var TaskScrub = Task{
 }
 
 func taskScrubRun(ctx context.Context, task *Task) (err error) {
-	err = errors.Join(err, taskScrubRunSvcStatus(ctx, task))
-	err = errors.Join(err, taskScrubRunResStatus(ctx, task))
-	err = errors.Join(err, taskScrubRunSvcInstances(ctx, task))
+	err = errors.Join(err, taskScrubObjects(ctx, task))
+	err = errors.Join(err, taskScrubResources(ctx, task))
+	err = errors.Join(err, taskScrubInstances(ctx, task))
 	return
 }
 
-func taskScrubRunSvcInstances(ctx context.Context, task *Task) error {
+func taskScrubInstances(ctx context.Context, task *Task) error {
 	return nil
 }
 
-func taskScrubRunResStatus(ctx context.Context, task *Task) error {
+func taskScrubResources(ctx context.Context, task *Task) error {
 	odb, err := task.DBX(ctx)
 	if err != nil {
 		return err
@@ -41,47 +40,48 @@ func taskScrubRunResStatus(ctx context.Context, task *Task) error {
 	defer odb.Rollback()
 
 	// Fetch the outdated resources still not in "undef" availstatus
-	ids, rids, svcIDs, nodeIDs, err := odb.ResourceOutdatedLists(ctx)
+	resources, err := odb.ResourceOutdatedLists(ctx)
 	if err != nil {
 		return err
 	}
 
-	n := len(rids)
+	n := len(resources)
 	if n == 0 {
 		return nil
 	}
+
 	names := make([]string, n)
 
 	// Historize `services` lines we will touch
-	for i, _ := range ids {
-		names[i] = fmt.Sprintf("%s@%s:%s", svcIDs[i], nodeIDs[i], rids[i])
-		if err := odb.ResourceUpdateLog(ctx, rids[i], svcIDs[i], nodeIDs[i], "undef"); err != nil {
+	for i, resource := range resources {
+		names[i] = resource.String()
+		if err := odb.ResourceUpdateLog(ctx, resource, "undef"); err != nil {
 			return err
 		}
 	}
 
 	// Update the `services` table
-	if n, err := odb.ResourceUpdateStatus(ctx, ids, "undef"); err != nil {
+	if modified, err := odb.ResourceUpdateStatus(ctx, resources, "undef"); err != nil {
 		return err
-	} else if int(n) != len(ids) {
-		task.Infof("set %d/%d resmon status to undef (no live instance) amongst %s", n, len(svcIDs), names)
+	} else if int(modified) != n {
+		task.Infof("set %d/%d resmon status to undef (no live instance) amongst %s", modified, n, names)
 	} else {
 		task.Infof("set %d resmon status to undef (no live instance) for %s", n, names)
 	}
 
 	// Create log table entries
 	logEntries := make([]cdb.LogEntry, n)
-	for i, _ := range ids {
+	for i, resource := range resources {
 		d := make(map[string]any)
-		d["name"] = names[i]
+		d["name"] = resource.String()
 		logEntries[i] = cdb.LogEntry{
 			Action: "resource.status",
 			Fmt:    "resource '%(name)s' status flagged 'undef'",
 			Dict:   d,
 			User:   "scheduler",
 			Level:  "error",
-			SvcID:  &svcIDs[i],
-			NodeID: &nodeIDs[i],
+			SvcID:  &resource.OID,
+			NodeID: &resource.NID,
 		}
 	}
 	if err := odb.Log(ctx, logEntries...); err != nil {
@@ -100,7 +100,7 @@ func taskScrubRunResStatus(ctx context.Context, task *Task) error {
 	return nil
 }
 
-func taskScrubRunSvcStatus(ctx context.Context, task *Task) error {
+func taskScrubObjects(ctx context.Context, task *Task) error {
 	odb, err := task.DBX(ctx)
 	if err != nil {
 		return err
